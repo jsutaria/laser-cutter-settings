0e6b5cf42b6c69c731aca3212899b9eb4f2a02ea
Jainil Sutaria <jsutaria@stanford.edu>
2019-09-13 15:24:50 -0700

    Add footer

---
+++ src/content/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Footer = () => {
+    return (
+        <FooterDiv>
+            Made with ❤ ️by <a href="https://jainilsutaria.com/">Jainil Sutaria</a>. Please suggest edits <a href="https://github.com/jsutaria/laser-cutter-settings">here</a>!
+        </FooterDiv>
+    )
+}
+
+const FooterDiv = styled.div`
+    width: 100%;
+    text-align: center;
+`
+
+export default Footer
